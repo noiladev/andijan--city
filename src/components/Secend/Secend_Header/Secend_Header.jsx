176502e64ec2c3b26a97bd3935b081814f581559
MrKarimov01 <tungiborilar094@gmail.com>
2024-02-23 14:49:12 +0500

    Rie bolim

---
+++ src/components/Secend/Secend_Header/Secend_Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Secend_Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Secend_Header
--- src/pages/Rieltoriy/Riel.jsx
+++ src/pages/Rieltoriy/Riel.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Riel() {
   return (
-    <div>Xusanboy</div>
+    <div>
+      https://www.figma.com/file/6suTnt2GxV4gqqGhvxBXbD/Andijan-city-(Copy)?type=design&node-id=70-3369&mode=design&t=SPjpdHHFgN1cz7mx-0
+    </div>
   )
 }
 
