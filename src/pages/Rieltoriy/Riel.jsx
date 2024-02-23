12f321f34e73594cc2951481ace6d96d3eb8f8cc
MrKarimov01 <tungiborilar094@gmail.com>
2024-02-23 14:50:30 +0500

    Rie bolim

--- src/pages/Rieltoriy/Riel.jsx
+++ src/pages/Rieltoriy/Riel.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 function Riel() {
   return (
-    <div>
-      https://www.figma.com/file/6suTnt2GxV4gqqGhvxBXbD/Andijan-city-(Copy)?type=design&node-id=70-3369&mode=design&t=SPjpdHHFgN1cz7mx-0
-    </div>
+    <div>Xusanboy</div>
   )
 }
 
