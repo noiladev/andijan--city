09033834201f6acab0788ae347e320cf10b7eaf7
Phantuming <imranrasulovgaming@gmail.com>
2024-03-01 13:38:06 +0500

    Third

--- src/pages/Third/Third.jsx
+++ src/pages/Third/Third.jsx
@@ -6,6 +6,8 @@ import card__img2 from '../../assets/img/card2.png'
 import card__img3 from '../../assets/img/card3.png'
 import pagination from '../../assets/img/pagination.png'
 import banner from '../../assets/img/Banner.png'
+import map from '../../assets/img/map.png'
+
 function Third() {
   return (
     <section className="Third">
@@ -14,45 +16,59 @@ function Third() {
         <div className="main">
           <div className="pagin">
             <p className="main__p">Особенности комплекса</p>
-              <img src={pagination} alt="" />
+            <img src={pagination} alt="" />
           </div>
-            <div className="cards">
-              <div className="card">
-                <img src={card__img3} alt="" />
-              </div>
-              <div className="card">
-                <img src={card__img2} alt="" />
-              </div>
-              <div className="card">
-                <img src={card__img3} alt="" />
-              </div>
-              <div className="card">
-                <img src={card__img} alt="" />
-              </div>
+          <div className="cards">
+            <div className="card">
+              <img src={card__img3} alt="" />
             </div>
-            <div className="pagin">
+            <div className="card">
+              <img src={card__img2} alt="" />
+            </div>
+            <div className="card">
+              <img src={card__img3} alt="" />
+            </div>
+            <div className="card">
+              <img src={card__img} alt="" />
+            </div>
+          </div>
+          <div className="pagin">
             <p className="main__p2">Места поблизости</p>
             <img className='img__pagin' src={pagination} alt="" />
 
+          </div>
+          <div className="cards">
+            <div className="card">
+              <img src={card__img2} alt="" />
+            </div>              <div className="card">
+              <img src={card__img} alt="" />
             </div>
-            <div className="cards">
-              <div className="card">
-                <img src={card__img2} alt="" />
-              </div>              <div className="card">
-                <img src={card__img} alt="" />
-              </div>
-              <div className="card">
-                <img src={card__img3} alt="" />
-              </div>
-              <div className="card">
-                <img src={card__img2} alt="" />
-              </div>
-              </div>
-              </div>
+            <div className="card">
+              <img src={card__img3} alt="" />
+            </div>
+            <div className="card">
+              <img src={card__img2} alt="" />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="bannner">
-                <img className='banner' src={banner} alt="" />
-              </div>
+        <img className='banner' src={banner} alt="" />
+      </div>
+      <div className="textd">
+        <div className="text">
+          <h3 className="t__h3">О жилом комплексе</h3>
+          <p className='t__p'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cumque totam voluptates <br />ea dignissimos sapiente ratione sunt amet nihil
+            laudantium ipsa dolore deleniti, assumenda quae consequuntur. <br />
+            <br />
+            Aliquid illum libero exercitationem cupiditate nulla hic?<br />
+            Porro molestias ducimus, vel exercitationem odit ea laudantium similique.
+            Cupiditate, maiores enim! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
