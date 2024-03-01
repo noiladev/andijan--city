8284d91b0e9d1852702e2267ca89c47437f41191
Phantuming <imranrasulovgaming@gmail.com>
2024-03-01 16:51:08 +0500

    The End

--- src/pages/Third/Third.jsx
+++ src/pages/Third/Third.jsx
@@ -6,59 +6,63 @@ import card__img2 from '../../assets/img/card2.png'
 import card__img3 from '../../assets/img/card3.png'
 import pagination from '../../assets/img/pagination.png'
 import banner from '../../assets/img/Banner.png'
-import map from '../../assets/img/map.png'
+import building from '../../assets/img/building.png'
+import Header from '../../components/Header/Header'
+import form from '../../assets/img/form.png'
 
 function Third() {
   return (
+    <div className="third__special">
     <section className="Third">
+      <Header/>
       <div className="container">
-        <img src={top} alt="" />
-        <div className="main">
-          <div className="pagin">
-            <p className="main__p">Особенности комплекса</p>
+        <img className='third__top' src={top} alt="" />
+        <div className="third__main">
+          <div className="third__pagin">
+            <p className="third__main__p">Особенности комплекса</p>
             <img src={pagination} alt="" />
           </div>
-          <div className="cards">
-            <div className="card">
+          <div className="third__cards">
+            <div className="third__card">
               <img src={card__img3} alt="" />
             </div>
-            <div className="card">
+            <div className="third__card">
               <img src={card__img2} alt="" />
             </div>
-            <div className="card">
+            <div className="third__card">
               <img src={card__img3} alt="" />
             </div>
-            <div className="card">
+            <div className="third__card">
               <img src={card__img} alt="" />
             </div>
           </div>
-          <div className="pagin">
-            <p className="main__p2">Места поблизости</p>
-            <img className='img__pagin' src={pagination} alt="" />
+          <div className="third__pagin">
+            <p className="third__main__p2">Места поблизости</p>
+            <img className='third__img__pagin' src={pagination} alt="" />
 
           </div>
-          <div className="cards">
-            <div className="card">
+          <div className="third__cards">
+            <div className="third__card">
               <img src={card__img2} alt="" />
             </div>              <div className="card">
               <img src={card__img} alt="" />
             </div>
-            <div className="card">
+            <div className="third__card">
               <img src={card__img3} alt="" />
             </div>
-            <div className="card">
+            <div className="third__card">
               <img src={card__img2} alt="" />
             </div>
           </div>
         </div>
       </div>
-      <div className="bannner">
-        <img className='banner' src={banner} alt="" />
+      <div className="third__bannner">
+        <img className='third__banner' src={banner} alt="" />
       </div>
-      <div className="textd">
-        <div className="text">
-          <h3 className="t__h3">О жилом комплексе</h3>
-          <p className='t__p'>
+      <div className="third__textd">
+        <div className="third__text">
+          <h3 className="third__t__h3">О жилом комплексе</h3>
+          <p className='third__t__p'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Cumque totam voluptates <br />ea dignissimos sapiente ratione sunt amet nihil
             laudantium ipsa dolore deleniti, assumenda quae consequuntur. <br />
@@ -69,7 +73,15 @@ function Third() {
           </p>
         </div>
       </div>
+      <div className="third__key">
+        <img src={form} alt="" />
+        <input className='third__input' type="text"  placeholder='Ваше имя'/>
+        <input className='third__input2' type="text"  placeholder='Ваш email'/>
+        <input className='third__input3' type="text"  placeholder='+998__ ___ __ __'/>
+        <button className='third__btn'>Отправить</button>
+        </div>
     </section>
+    </div>
   )
 }
 
