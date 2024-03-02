ecb33177600a681961be29f095c1c0664ab1e0a4
oyatillo0 <omrzayv@gmail.com>
2024-03-02 15:04:38 +0500

    dhasdguhdfg

--- src/components/Secend/Secend_Headr/Secend_Headr.jsx
+++ src/components/Secend/Secend_Headr/Secend_Headr.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Secend_Headr.scss'
 import Header from '../../Header/Header'
 import bottomimg from '../../../assets/Secend_imgs/btn__bottom_img.png'
+import btnimg from '../../../assets/Secend_imgs/btn__img.png'
 
 function Secend_Headr() {
   return (
     <div className='Header'>
-          <Header/>
+      <Header />
       <div className="container">
         <div className="Header__nav">
           <div className="Header__nav__btns">
@@ -15,17 +16,17 @@ function Secend_Headr() {
               <div class="dropdown">
                 <h6>Выберите проект</h6>
                 <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Любой
+                  Любой
                 </button>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Любой</a></li>
-                  <li><a class="dropdown-item" href="#">Любой</a></li>
+                  <li><a class="dropdown-item" href="#">современный</a></li>
+                  <li><a class="dropdown-item" href="#">простой</a></li>
                   <li><a class="dropdown-item" href="#">Любой</a></li>
                 </ul>
               </div><div class="dropdown">
                 <h6>Выберите проект</h6>
                 <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Любой
+                  Любой
                 </button>
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item" href="#">современный</a></li>
@@ -37,22 +38,41 @@ function Secend_Headr() {
                 <h6 className='btn__h6'>Укажите площадь дома, м²</h6>
                 <div className="house__size__inp">
                   <h6 className='house__size__h6'>от</h6>
-                  <input className='house__size__inp1' type="number" placeholder='00,0'/>
+                  <input className='house__size__inp1' type="number" placeholder='00,0' />
                   <span className='input__line'></span>
                   <h6 className='house__size__h6' >до</h6>
-                  <input className='house__size__inp1' type="number" placeholder='00,0'/>
+                  <input className='house__size__inp1' type="number" placeholder='00,0' />
                 </div>
                 <img className='btn__botom__img' src={bottomimg} alt="" />
               </div>
-              <div className="btn"></div>
+              <div className="Nav__btns__and__lorm">
+                <div className="Nav__btns__drop">
+                  <div className="Nav__btns__drop1">
+                    <img src={btnimg} alt="" />
+                  </div>
+                  <div className="Nav__btns__drop2">
+                    <div className="Nav__btn__w">
+                    <div className="house__size__inp">
+                      <h6 className='house__size__h6'>от</h6>
+                      <input className='house__size__inp1' type="number" placeholder='00,0' />
+                      <h6 className='house__size__h6' >uzs</h6>
+                      <span className='input__line'></span>
+                      <h6 className='house__size__h6' >до</h6>
+                      <input className='house__size__inp1' type="number" placeholder='00,0' />
+                      <h6 className='house__size__h6' >uzs</h6>
+                    </div>
+                    <img className='btn__botom__img2' src={bottomimg} alt="" />
+                  </div></div>
+                </div>
+              </div>
               <div class="dropdown">
                 <h6>Выберите проект</h6>
                 <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Любой
+                  Любой
                 </button>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Любой</a></li>
-                  <li><a class="dropdown-item" href="#">Любой</a></li>
+                  <li><a class="dropdown-item" href="#">современный</a></li>
+                  <li><a class="dropdown-item" href="#">простой</a></li>
                   <li><a class="dropdown-item" href="#">Любой</a></li>
                 </ul>
               </div>
