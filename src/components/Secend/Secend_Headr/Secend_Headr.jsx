b5e76645a753978562e06049531288ce4005b945
oyatillo0 <omrzayv@gmail.com>
2024-03-06 10:55:49 +0300

    astsuyfgd

--- src/components/Secend/Secend_Headr/Secend_Headr.jsx
+++ src/components/Secend/Secend_Headr/Secend_Headr.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './Secend_Headr.scss'
 import Header from '../../Header/Header'
 import bottomimg from '../../../assets/Secend_imgs/btn__bottom_img.png'
-import btnimg from '../../../assets/Secend_imgs/btn__img.png'
+import smalicon from '../../../assets/Secend_imgs/btn__smal__img.png'
+import semalicon from '../../../assets/Secend_imgs/btn__smal__img_grey.png'
+import btnbotimg from '../../../assets/Secend_imgs/btn__botom__img.png'
+import rightximg from '../../../assets/Secend_imgs/h5__x__img.png'
+import btnico from '../../../assets/Secend_imgs/btn__icon.png'
 
 function Secend_Headr() {
   return (
@@ -48,21 +52,22 @@ function Secend_Headr() {
               <div className="Nav__btns__and__lorm">
                 <div className="Nav__btns__drop">
                   <div className="Nav__btns__drop1">
-                    <img src={btnimg} alt="" />
+                    <h6 className='Nav__btns__drop1__h61'>По стоимости <img className='Nav__btns__drop1__h6_icon' src={smalicon} alt="" /></h6>
+                    <h6 className='Nav__btns__drop1__h6'>По взносу <img className='Nav__btns__drop1__h6_icon' src={semalicon} alt="" /></h6>
+                    <h6 className='Nav__btns__drop1__h6'>По платежу <img className='Nav__btns__drop1__h6_icon' src={semalicon} alt="" /></h6>
                   </div>
                   <div className="Nav__btns__drop2">
-                    <div className="Nav__btn__w">
-                    <div className="house__size__inp">
-                      <h6 className='house__size__h6'>от</h6>
-                      <input className='house__size__inp1' type="number" placeholder='00,0' />
-                      <h6 className='house__size__h6' >uzs</h6>
-                      <span className='input__line'></span>
-                      <h6 className='house__size__h6' >до</h6>
-                      <input className='house__size__inp1' type="number" placeholder='00,0' />
-                      <h6 className='house__size__h6' >uzs</h6>
+                    <div className="drop2__btns__inp">
+                      <h6 className='drop2__h6'>от</h6>
+                      <input className='drop2__inp' type="number" placeholder="0000" />
+                      <h6 className='drop2__h61'>uzs</h6>
+                      <span className='drop2__line'></span>
+                      <h6 className='drop2__h62'>до</h6>
+                      <input className='drop2__inp' type="number" placeholder="0000" />
+                      <h6 className='drop2__h63'>uzs</h6>
                     </div>
-                    <img className='btn__botom__img2' src={bottomimg} alt="" />
-                  </div></div>
+                    <img className='Nav__btns__drop2__btm__img' src={btnbotimg} alt="" />
+                  </div>
                 </div>
               </div>
               <div class="dropdown">
@@ -76,6 +81,11 @@ function Secend_Headr() {
                   <li><a class="dropdown-item" href="#">Любой</a></li>
                 </ul>
               </div>
+            </div>
+            <div className="Nav__thre__btns">
+              <button className='thre__btn'> <img src={btnico} alt="" />Все фильтры</button>
+              <h3 className='thre__h3'>Найдено 84 дома</h3>
+              <h5 className='thre__h5'>Очистить фильтр <img src={rightximg} alt="" /></h5>
             </div>
           </div>
         </div>
--- src/pages/Second/Second.jsx
+++ src/pages/Second/Second.jsx
@@ -3,13 +3,15 @@ import './Second.scss'
 import Secend_Headr from '../../components/Secend/Secend_Headr/Secend_Headr'
 import Secend_Mein from '../../components/Secend/Secend_Mein/Secend_Mein'
 import Secend_Midle from '../../components/Secend/Secend_Midle/Secend_Midle'
+import Secend_Footer from '../../components/Secend/Secend_Footer/Secend_Footer'
 
 function Second() {
   return (
-    <div>
+    <div className='Second'>
       <Secend_Headr/>
       <Secend_Mein/>
       <Secend_Midle/>
+      <Secend_Footer/>
     </div>
   )
 }
